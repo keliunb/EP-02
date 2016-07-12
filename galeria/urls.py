6a77d05ce7de78da0a31b5991b468eb430eb6f1a
Keli Cristina Borges <keliunb@gmail.com>
2016-07-11 20:56:53 -0700

    include app galeria

---
+++ galeria/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('galeria.views',
+    url(r'^$', 'albuns', name='albuns'),
+    url(r'^(?P<slug>[\w_-]+)/$', 'album', name='album'),
+)
